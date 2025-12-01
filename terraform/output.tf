125aa90b7e5aee597b086cfd8796b1b0052988ed
Vijith Ayyanchira mohanan <vijithmohan@Vijiths-MacBook-Pro.local>
2025-12-01 13:24:58 +0000

    new updated the jenkinsfile

--- terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_name" {
-  description = "The name of the EKS cluster."
-  value       = module.eks.cluster_id
-}
-
-#output "kubeconfig" {
-  #description = "The AWS command to update your local kubeconfig file."
-  #value       = "aws eks update-kubeconfig --name ${module.eks.cluster_id} --region ${var.aws_region}"
-#}
